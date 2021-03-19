49ae0d2144d096b0544938fae6ff74d31321a7db
Jack Kiley <johnekiley@gmail.com>
2021-03-19 22:14:04 +0000

    JK: More docs

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,7 +5,7 @@ with stg_customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
 )
 
 select * from stg_customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -6,7 +6,7 @@ with stg_orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ source('jaffle_shop', 'orders') }}
 )
 
 select * from stg_orders
